3ac7dbee8b0dcb8d6abc642ef106434ea99d80b6
Jose Fernando <pepernando@gmail.com>
2021-11-18 20:12:32 -0600

    borrar funcionando

--- backend/salas.js
+++ backend/salas.js
@@ -4,6 +4,7 @@ var router = express.Router();
 //Toda la funcionalidad de las reservaciones
 var db = require('./basedeDatos')
 
+//pagina principal de sala de juntas
 router.get('/',(req,res) => {
     let arraux = []
     db.each("SELECT * FROM salas", (err, row) => {
@@ -16,12 +17,22 @@ router.get('/',(req,res) => {
         res.render('salas',{array: arraux})
     })
 })
-
+// Agregar una sala de juntas
 router.post('/',(req,res) => {
     const obj = JSON.parse(JSON.stringify(req.body)); 
     console.log(obj)
     db.serialize( () => {
         db.run('INSERT INTO salas(nombre,disponibilidad) VALUES (?,1)',obj.nombresala)
+    })
+    res.redirect('/salas')
+})
+
+//borrar sala
+router.get('/borrar/:id',(req,res) => {
+    let id = req.params.id
+    console.log('id es',id)
+    db.serialize( () => {
+        db.run('delete from salas where idsala=?',id)
     })
     res.redirect('/salas')
 })
